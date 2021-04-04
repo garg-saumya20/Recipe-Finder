d9ef956db11155b9d34b8169021dab962faf2c92
Saumya Garg <garg.saumya20@gmail.com>
2021-04-04 19:54:19 +0530

    Change

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ SearchForRecipe('chicken');
             return (
               <div  key={item.recipe.label} className="Ingredient">
                 <span>{item.recipe.label}</span>
-                <img src={item.recipe.image}/>
+                <img src={item.recipe.image} alt="item"/>
                 <div  className="Steps">
                 {item.recipe.ingredientLines.map((step,index)=>{
                       return <p key={index}>{step}</p>;
